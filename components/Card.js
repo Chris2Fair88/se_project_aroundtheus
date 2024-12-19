657ba5045f40b6736f69b1ac96e93545de8a93c4
Chris Fairbanks <Chris2Fair88@gmail.com>
2024-12-19 13:19:12 -0500

    refactor cardElement and likeButton

--- components/Card.js
+++ components/Card.js
@@ -7,7 +7,9 @@ export default class Card {
     _setEventListeners() {
         this._cardElement
             .querySelector(".card__like-button")
-            .addEventListener(click, () => {this.});
+            .addEventListener(click, () => {
+                likeButton.classList.toggle("card__like-button_active");
+            });
         const deleteButton =
             this._cardElement.querySelector(".card__like-button");
     }
